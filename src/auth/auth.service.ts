d18e54aef4fa0e58deff673f3180c4c083b97a5d
Zachary Schwartz <zachscs@outlook.com>
2018-05-25 16:17:04 -0700

    formatted

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,28 +7,35 @@ import { User } from '../user/interfaces/user.interface';
 
 @Injectable()
 export class AuthService {
-
   constructor(private readonly userService: UserService) {}
 
   async login(authDto: AuthDto) {
     const user = await this.userService.findOne(authDto);
     if (!user) {
-      return {error: 'User not found!'};
+      return { error: 'User not found!' };
     }
     const match = await bcrpyt.compare(authDto.password, user.password);
     if (match) {
-      const token = {token: jwt.sign({
-        username: authDto.username,
-        exp: Math.round(new Date().getTime() / 1000) + 604800,
-      }, 'secret')};
+      const token = {
+        token: jwt.sign(
+          {
+            username: authDto.username,
+            exp: Math.round(new Date().getTime() / 1000) + 604800,
+          },
+          'secret',
+        ),
+      };
       return token;
     }
-    return {error: 'Invalid password'};
+    return { error: 'Invalid password' };
   }
 
   async signup(authDto: AuthDto) {
     const hash = await bcrpyt.hash(authDto.password, 1);
-    const res = await this.userService.create({username: authDto.username, password: hash});
-    return {message: 'success'};
+    const res = await this.userService.create({
+      username: authDto.username,
+      password: hash,
+    });
+    return { message: 'success' };
   }
 }
