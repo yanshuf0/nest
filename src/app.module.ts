b4cc986a6f7de27bf1a4afea254942a3bb2ed205
Zachary Schwartz <zachscs@outlook.com>
2018-05-25 13:35:05 -0700

    better tasks settings mongo start

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost/fridgeit')],
   controllers: [AppController],
   providers: [AppService],
 })
